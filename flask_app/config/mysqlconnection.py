3d17203ee79e49372019dd9713f4f4109d17483c
terravidhal <infiniterraetincelle@gmail.com>
2023-09-17 17:47:06 +0100

    delete mp db

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -7,7 +7,7 @@ class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', # change the user and password as needed
-                                    password = 'jloveinfini25', 
+                                    password = '', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
